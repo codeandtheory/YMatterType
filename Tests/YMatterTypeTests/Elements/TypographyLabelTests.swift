43b9e6ee0c8ee4dffa83cfb735686e39ec72f9f9
Mark Pospesel <mark.pospesel@ymedialabs.com>
2022-09-21 14:42:30 +0200

    Fix linter violations

--- Tests/YMatterTypeTests/Elements/TypographyLabelTests.swift
+++ Tests/YMatterTypeTests/Elements/TypographyLabelTests.swift
@@ -56,7 +56,10 @@ final class TypographyLabelTests: TypographyElementTests {
             sut.text = array.joined(separator: "\n")
 
             // we expect label height to be a multiple of lineHeight + paragraph spacing
-            XCTAssertEqual(sut.intrinsicContentSize.height, sut.typography.lineHeight * CGFloat($0) + spacing * CGFloat($0 - 1))
+            XCTAssertEqual(
+                sut.intrinsicContentSize.height,
+                sut.typography.lineHeight * CGFloat($0) + spacing * CGFloat($0 - 1)
+            )
             // after calling sizeToFit we expect bounds to equal intrinsicContentSize
             sut.sizeToFit()
             XCTAssertEqual(sut.intrinsicContentSize, sut.bounds.size)
