d22f8cadd2c89eb65789391a72c0c0549969e7ba
virginia.pujols <virginia.pujols@ymedialabs.com>
2023-01-26 13:20:25 -0400

    Adding unit tests and documentation

--- Sources/YMatterType/Elements/TextStyleLabel.swift
+++ Sources/YMatterType/Elements/TextStyleLabel.swift
@@ -8,44 +8,66 @@
 
 import SwiftUI
 
+/// A singe line text label that supports `Typography` for SwiftUI.
 public struct TextStyleLabel: View {
-    private let text: String
-    private let typography: Typography
-    private let configureTypographyText: ((TypographyLabel) -> Void)?
+    /// The text that the label displays.
+    let text: String
+    
+    /// Typography to be used for this label's text
+    let typography: Typography
+    
+    /// A closure that gets called on the init and refresh of the View
+    /// This closure allows you to provide additional configuration to the `TypographyLabel`
+    let configureTextStyleLabel: ((TypographyLabel) -> Void)?
 
-    public init(_ text: String,
-                typography: Typography,
-                configuration: ((TypographyLabel) -> Void)? = nil) {
-        self.typography = typography
-        self.text = text
-        self.configureTypographyText = configuration
+    /// The content and behavior of the view.
+    public var body: some View {
+        TypographyLabelRepresentable(
+            text: text,
+            typography: typography,
+            configureTextStyleLabel: configureTextStyleLabel
+        )
+        .fixedSize(horizontal: false, vertical: true)
     }
 
-    public var body: some View {
-        TypographyLabelRepresentable(text,
-                                     typography: typography,
-                                     configuration: configureTypographyText)
-            .fixedSize(horizontal: false, vertical: true)
+    /// Initializes a `TextStyleLabel` instance with the specified parameters
+    /// - Parameters:
+    ///   - text: The text that the label displays
+    ///   - typography: Typography to be used for this label's text
+    ///   - configuration: A closure that gets called on the init and refresh of the View
+    public init(
+        _ text: String,
+        typography: Typography,
+        configuration: ((TypographyLabel) -> Void)? = nil
+    ) {
+        self.typography = typography
+        self.text = text
+        self.configureTextStyleLabel = configuration
     }
 }
 
-public extension TextStyleLabel {
+extension TextStyleLabel {
+    /// A wrapper for a UIKit TypographyLabel view that allows to integrate that view into a SwiftUI view hierarchy.
     struct TypographyLabelRepresentable: UIViewRepresentable {
-        public typealias UIViewType = TypographyLabel
+        /// The type of view to present.
+        typealias UIViewType = TypographyLabel
 
-        private let typography: Typography
-        private let text: String
-        private let configureText: ((TypographyLabel) -> Void)?
-
-        public init(_ text: String,
-                    typography: Typography,
-                    configuration: ((TypographyLabel) -> Void)? = nil) {
-            self.typography = typography
-            self.text = text
-            self.configureText = configuration
-        }
+        /// The text that the label displays.
+        let text: String
         
-        public func makeUIView(context: Context) -> TypographyLabel {
+        /// Typography to be used for this label's text
+        let typography: Typography
+        
+        /// A closure that gets called on the init and refresh of the View
+        let configureTextStyleLabel: ((TypographyLabel) -> Void)?
+
+        /// Creates the view object and configures its initial state.
+        ///
+        /// - Parameter context: A context structure containing information about
+        ///   the current state of the system.
+        ///
+        /// - Returns: `TypographyLabel` view configured with the provided information.
+        func makeUIView(context: Context) -> TypographyLabel {
             let label = TypographyLabel(typography: typography)
             label.text = text
             
@@ -54,14 +76,21 @@ public extension TextStyleLabel {
             label.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
             label.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
 
-            configureText?(label)
+            configureTextStyleLabel?(label)
             return label
         }
         
-        public func updateUIView(_ uiView: TypographyLabel, context: Context) {
+        /// Updates the state of the specified view with new information from
+        /// SwiftUI.
+        ///
+        /// - Parameters:
+        ///   - uiView: `TypographyLabel` view.
+        ///   - context: A context structure containing information about the current
+        ///     state of the system.
+        func updateUIView(_ uiView: TypographyLabel, context: Context) {
             uiView.typography = typography
             uiView.text = text
-            configureText?(uiView)
+            configureTextStyleLabel?(uiView)
         }
     }
 }
